f91f3a82669743eea352272584c3aa8e927a0762
Prachi2402 <99326303+Prachi2402@users.noreply.github.com>
2023-05-03 20:19:31 +0530

    Delete Sum square difference.js

--- Sum square difference.js
+++
@@ -1,7 +0,0 @@
-function sumSquareDifference(n) {
-  const sumOfN = (n*(n+1))/2;
-  const sumOfNSquare = (n*(n+1)*(2*n+1))/6;
-  console.log(sumOfN**2 - sumOfNSquare)
-  return (sumOfN ** 2) - sumOfNSquare;
-}
-sumSquareDifference(20)
